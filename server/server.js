e3058aad8b7a9befe7752ad7b4aae1fd52af5126
Ravinder <ravinder.s.negi@indusnet.co.in>
2023-12-01 17:29:13 +0530

    cors removed

--- server/server.js
+++ server/server.js
@@ -20,13 +20,7 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/", AuthenticationRoutes);
