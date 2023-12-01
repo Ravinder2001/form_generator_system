a8502640df5c6c3da3b594a9b917b7bed36cdb38
Ravinder <ravinder.s.negi@indusnet.co.in>
2023-12-01 17:25:53 +0530

    rate limiter added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { rateLimit } = require("express-rate-limit");
 
 const db = require("./config/db");
 const PORT = process.env.PORT || 5000;
@@ -8,7 +9,16 @@ const PORT = process.env.PORT || 5000;
 const AuthenticationRoutes = require("./routes/authentication.routes");
 const FormRoutes = require("./routes/form.routes");
 const { DOMAIN } = require("./utils/config");
-console.log("DOMAIN:", DOMAIN)
+console.log("DOMAIN:", DOMAIN);
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  limit: 20,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+});
+
+app.use(limiter);
 
 app.use(
   cors({
