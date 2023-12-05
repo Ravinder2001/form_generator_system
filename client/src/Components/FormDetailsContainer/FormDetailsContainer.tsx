7b260cd4d9ad5c3c1366b89d4367d26975eeaef9
Ravinder <ravinder.s.negi@indusnet.co.in>
2023-12-05 17:38:26 +0530

    new format

--- client/src/Components/FormDetailsContainer/FormDetailsContainer.tsx
+++ client/src/Components/FormDetailsContainer/FormDetailsContainer.tsx
@@ -47,38 +47,112 @@ function FormDetailsContainer(props: Props) {
         {!loading ? (
           <>
             {formDetails ? (
-              <table className={styles.table}>
-                <tbody className={styles.tbody}>
-                  {Data.map((item) => {
-                    let newFromdate;
-                    let newUptodate;
-                    if (item.name == "form_valid_from") {
-                      newFromdate = timestampToDateString(Number(formDetails.form_valid_from));
-                    }
-                    if (item.name == "form_valid_upto") {
-                      newUptodate = timestampToDateString(Number(formDetails.form_valid_upto));
-                    }
-
-                    return isMobile ? (
-                      <tr key={item.name} className={styles.tr}>
-                        <td className={styles.td}>{(formDetails as any)?.[item?.name]}</td>
-                        <td className={`${styles.td} ${styles.label}`}>{item?.label}</td>
-                      </tr>
-                    ) : (
-                      <tr key={item.name} className={styles.tr}>
-                        <td className={`${styles.td} ${styles.label}`}>{item?.label}</td>
-                        <td className={styles.td}>
-                          {item.name == "form_valid_from"
-                            ? newFromdate
-                            : item.name == "form_valid_upto"
-                            ? newUptodate
-                            : (formDetails as any)?.[item?.name]}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
+              <div className={styles.main_container}>
+                <div className={styles.box}>
+                  <div className={styles.label}>Form Type :</div>
+                  <div className={styles.value}>{formDetails?.form_type}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Form No :</div>
+                  <div className={styles.value}>{formDetails?.form_no}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Movement Type :</div>
+                  <div className={styles.value}>{formDetails?.movement_type}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Owner Name :</div>
+                  <div className={styles.value}>{formDetails?.owner_name}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Firm Name :</div>
+                  <div className={styles.value}>{formDetails?.firm_name}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Lease Address :</div>
+                  <div className={styles.value}>{formDetails?.lease_address}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Seller Reg. No :</div>
+                  <div className={styles.value}>{formDetails?.seller_reg_no}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Seller GSTIN :</div>
+                  <div className={styles.value}>{formDetails?.seller_gst}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Seller Address :</div>
+                  <div className={styles.value}>{formDetails?.seller_address}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Mineral Name :</div>
+                  <div className={styles.value}>{formDetails?.mineral_name}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Weight (Tons) :</div>
+                  <div className={styles.value}>{formDetails?.weight}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Sale Value (GST inclusive) :</div>
+                  <div className={styles.value}>{formDetails?.sale_value}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>GST Rate :</div>
+                  <div className={styles.value}>{formDetails?.gst_rate}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Payble GST :</div>
+                  <div className={styles.value}>{formDetails?.payble_gst}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Purchaser Reg. Status :</div>
+                  <div className={styles.value}>{formDetails?.purchase_status}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Purchaser Reg. No :</div>
+                  <div className={styles.value}>{formDetails?.purchase_reg_no}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Purchaser GSTIN :</div>
+                  <div className={styles.value}>{formDetails?.purchase_gst}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Destination Address :</div>
+                  <div className={styles.value}>{formDetails?.destination_address}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Vehicle Type :</div>
+                  <div className={styles.value}>{formDetails?.vehicle_type}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Vehicle Reg No :</div>
+                  <div className={styles.value}>{formDetails?.vehicle_reg_no}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Driver Name :</div>
+                  <div className={styles.value}>{formDetails?.driver_name}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Driver Mobile No :</div>
+                  <div className={styles.value}>{formDetails?.driver_no}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Travel Time (Hours) :</div>
+                  <div className={styles.value}>{formDetails?.travel_time}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Travel Distance KM :</div>
+                  <div className={styles.value}>{formDetails?.travel_distance}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Form Valid From :</div>
+                  <div className={styles.value}>{timestampToDateString(Number(formDetails.form_valid_from))}</div>
+                </div>
+                <div className={styles.box}>
+                  <div className={styles.label}>Form Valid Upto :</div>
+                  <div className={styles.value}>{timestampToDateString(Number(formDetails.form_valid_upto))}</div>
+                </div>
+              </div>
             ) : (
               <div>No data Found!</div>
             )}
