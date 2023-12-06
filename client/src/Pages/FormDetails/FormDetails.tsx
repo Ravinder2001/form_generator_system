ba5eddf893c0df8ef5cfa371cca61e25b0ee9969
Ravinder <ravinder.s.negi@indusnet.co.in>
2023-12-06 16:30:52 +0530

    changes

--- client/src/Pages/FormDetails/FormDetails.tsx
+++ client/src/Pages/FormDetails/FormDetails.tsx
@@ -3,11 +3,13 @@ import { useLocation } from "react-router-dom";
 import styles from "./style.module.css";
 
 import Middle from "../../Assets/Images/Middle.png";
+import Mob_Middle from "../../Assets/Images/mob_middle.png";
 import Footer from "../../Assets/Images/Footer.png";
+import Mob_Footer from "../../Assets/Images/mob_footer.png";
 import FormDetailsContainer from "../../Components/FormDetailsContainer/FormDetailsContainer";
 import FormDetailsHeader from "../../Components/FormDetailsHeader/FormDetailsHeader";
 import FormDetailsNavbar from "../../Components/FormDetailsNavbar/FormDetailsNavbar";
-import neww from "../../Assets/Images/newww.png"
+import neww from "../../Assets/Images/newww.png";
 function FormDetails() {
   const location = useLocation();
   const [isScroll, setIsScroll] = useState(false);
@@ -44,15 +46,15 @@ function FormDetails() {
   }, []);
   return (
     <div className={styles.container}>
-      {isScroll ? <FormDetailsNavbar /> : isMobile?<img src={neww} alt="" className={styles.new_img} />: <FormDetailsHeader />}
+      {isScroll ? <FormDetailsNavbar /> : isMobile ? <img src={neww} alt="" className={styles.new_img} /> : <FormDetailsHeader />}
 
       <FormDetailsContainer id={location.pathname.split("/")[3]} isMobile={isMobile} />
 
       <div className={styles.middle}>
-        <img src={Middle} alt="" className={styles.img} />
+        {isMobile ? <img src={Mob_Middle} alt="" className={styles.img} /> : <img src={Middle} alt="" className={styles.img} />}
       </div>
       <div className={styles.footer}>
-        <img src={Footer} alt="" className={styles.img} />
+        {isMobile ? <img src={Mob_Footer} alt="" className={styles.img} /> : <img src={Footer} alt="" className={styles.img} />}
       </div>
     </div>
   );
